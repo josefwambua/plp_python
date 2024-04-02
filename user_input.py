059701ac1e246f7dc62c4e40df8c619a05e1c6a0
Joseph Wambua <josefwambua2@gmail.com>
2024-04-02 14:52:59 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,11 @@
+# Ask for user's name
+name = input("What is your name? ")
+
+# Input users age
+age = input("How old are you? ")
+
+# Input user  location
+location = input("Where are you located? ")
+
+# Print the message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
